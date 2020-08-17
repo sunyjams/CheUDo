3343aca20fd8596d31574a3ca99f60f537b9e1c2
james <sunyjams@hotmail.com>
2020-08-17 10:47:21 +0800

    Initial commit

---
+++ app/src/main/java/com/xiaou/cheudo/App.java
@@ -0,0 +1,16 @@
+package com.xiaou.cheudo;
+
+import android.app.Application;
+
+/**
+ * @copyright: author:James
+ * date:2020/8/17
+ * description
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
